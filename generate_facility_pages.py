e63d33ae76e749c05a70b6c02bb0d20387f17275
SHF <shf@Jamess-MacBook-Air.local>
2026-01-30 02:08:17 -0500

    Remove SNF logo from facility detail page header

--- generate_facility_pages.py
+++ generate_facility_pages.py
@@ -796,7 +796,6 @@ def generate_html(ccn, p, quality_measures, penalties, surveys, wages):
 <svg width="18" height="18" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"/></svg>
 Back to Compare Tool
 </a>
-<a href="../snf-compare-landing.html" class="logo"><span class="logo-badge">SNF</span> compare</a>
 </div>
 </div>
 
